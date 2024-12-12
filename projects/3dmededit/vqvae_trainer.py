eace4c92fa527f811fbea926cc8191eae0404ee8
demirarikan <dmrarikan@gmail.com>
2024-12-12 16:59:01 +0100

    added test set evaluation to trainer

--- projects/3dmededit/vqvae_trainer.py
+++ projects/3dmededit/vqvae_trainer.py
@@ -4,13 +4,20 @@ import wandb
 import torch
 import os
 import logging
-
+import numpy as np
 
 class VQVAETrainer(Trainer):
     def __init__(self, training_params, model, data, device, log_wandb=True):
         super().__init__(training_params, model, data, device, log_wandb)
         self.val_interval = training_params["val_interval"]
         self.save_interval = training_params["save_interval"]
+
+        self.test_res_dir = training_params["test_res_dir"]
+        try:
+            self.test_ds = data.test_dataloader()
+        except:
+            logging.info("[VQVAETrainer::init]: Error loading test dataset")
+            self.test_ds = None
 
     def train(self, model_state=None, opt_state=None, start_epoch=0):
         if model_state is not None:
@@ -86,51 +93,119 @@ class VQVAETrainer(Trainer):
                     os.path.join(self.client_path, f"epoch_{epoch}.pt"),
                 )
             
-            if epoch % self.val_interval == 0 :
+            if epoch % self.val_interval == 0 and epoch != 0:
                 self.test(
                     model_weights=self.model.state_dict(),
                     test_data=self.val_ds,
-                    task="val",
+                    task="Val",
                     optimizer_weights=self.optimizer.state_dict(),
                     epoch=epoch,
                 )
+        
+        if self.test_ds:
+            self.test(
+                model_weights=self.best_weights,
+                test_data=self.test_ds,
+                task="Test",
+                optimizer_weights=self.best_opt_weights,
+                epoch=-1,
+            )
+
         return self.best_weights, self.best_opt_weights
     
     def test(self, model_weights, test_data, task="Val", optimizer_weights=None, epoch=0):
         self.model.load_state_dict(model_weights)
         self.model.eval()
-        val_loss = 0
-        with torch.no_grad():
-            for batch_idx, (volume, _) in enumerate(test_data):
-                volume = volume.to(self.device)
-                reconstruction, quant_loss = self.model(volume)
 
-                if batch_idx == 1:
-                    wandb.log({
-                        "reconstruction": wandb.Image(reconstruction[0, 0, 100].cpu().detach().numpy() * 255),
-                        "original": wandb.Image(volume[0, 0, 100].cpu().detach().numpy() * 255),
-                    })
+        if task == "Val":
+            logging.info("[Trainer::test]: Started validation")
+
+            val_loss = 0
+            with torch.no_grad():
+                for batch_idx, (volume, _) in enumerate(test_data):
+                    volume = volume.to(self.device)
+                    reconstruction, quant_loss = self.model(volume)
+
+                    if batch_idx == 1:
+                        wandb.log({
+                            "reconstruction": wandb.Image(reconstruction[0, 0, 100].cpu().detach().numpy() * 255),
+                            "original": wandb.Image(volume[0, 0, 100].cpu().detach().numpy() * 255),
+                        })
 
 
-                recon_loss = self.criterion_rec(volume, reconstruction)
-                val_loss += recon_loss.item()
-        val_loss /= len(test_data)
-        if val_loss < self.min_val_loss:
-            self.min_val_loss = val_loss
+                    recon_loss = self.criterion_rec(volume, reconstruction)
+                    val_loss += recon_loss.item()
+            val_loss /= len(test_data)
+            if val_loss < self.min_val_loss:
+                self.min_val_loss = val_loss
 
-            torch.save(
-                {
-                    "model_weights": model_weights,
-                    "optimizer_weights": optimizer_weights,
-                    "epoch": epoch,
-                },
-                os.path.join(self.client_path, "best.pt"),
+                torch.save(
+                    {
+                        "model_weights": model_weights,
+                        "optimizer_weights": optimizer_weights,
+                        "epoch": epoch,
+                    },
+                    os.path.join(self.client_path, f"best_epoch_{epoch}.pt"),
+                )
+
+                self.best_weights = model_weights
+                self.best_opt_weights = optimizer_weights
+            wandb.log({f"{task}_loss": val_loss, "epoch": epoch})
+            logging.info(
+                f"[Trainer::test]: {task} loss: {val_loss}"
             )
+            return val_loss
+        
+        elif task == "Test":
+            logging.info("[Trainer::test]: Started testing")
 
-            self.best_weights = model_weights
-            self.best_opt_weights = optimizer_weights
-        wandb.log({f"{task}_loss": val_loss, "epoch": epoch})
-        logging.info(
-            f"[Trainer::test]: {task} loss: {val_loss}"
-        )
-        return val_loss
+            total_l1_loss = 0
+            total_mse_loss = 0
+            num_samples = 0
+
+            with torch.no_grad():
+                for batch_idx, (volume, _) in enumerate(test_data):
+                    volume = volume.to(self.device)
+                    reconstruction, quant_loss = self.model(volume)
+
+                    batch_size = volume.size(0)  # Get batch size
+                    l1_loss = torch.nn.functional.l1_loss(reconstruction, volume, reduction='sum')  
+                    mse_loss = torch.nn.functional.mse_loss(reconstruction, volume, reduction='sum')
+
+                    total_l1_loss += l1_loss.item()
+                    total_mse_loss += mse_loss.item()
+                    num_samples += batch_size
+
+            
+                    for i in range(batch_size):
+                        wandb.log({
+                                "reconstruction_slice_1": wandb.Image(reconstruction[i, 0, 100].cpu().detach().numpy() * 255),
+                                "reconstruction_slice_2": wandb.Image(reconstruction[i, 0, :, 75, :].cpu().detach().numpy() * 255),
+                                "reconstruction_slice_3": wandb.Image(reconstruction[i, 0, :, :, 75].cpu().detach().numpy() * 255),
+                                "original_slice_1": wandb.Image(volume[i, 0, 100].cpu().detach().numpy() * 255),
+                                "original_slice_2": wandb.Image(volume[i, 0, :, 75, :].cpu().detach().numpy() * 255),
+                                "original_slice_3": wandb.Image(volume[i, 0, :, :, 75].cpu().detach().numpy() * 255),
+                            })
+
+
+                    for i, recon_scan in enumerate(reconstruction):
+                        np.save(
+                            os.path.join(self.test_res_dir, f"recon_batch_{batch_idx}_{i}.npy"),
+                            recon_scan.cpu().detach().numpy(),
+                        )
+
+            average_l1_loss = total_l1_loss / num_samples
+            average_mse_loss = total_mse_loss / num_samples
+
+            results = {
+                "test_l1": average_l1_loss, 
+                "test_mse_loss": average_mse_loss,
+            }
+
+            wandb.log(results)
+
+        
+    def compute_metrics(self, volume, reconstruction):
+        l1_loss = torch.nn.functional.l1_loss(volume, reconstruction, reduction="sum")
+        mse_loss = torch.nn.functional.mse_loss(volume, reconstruction, reduction="sum")
+        return l1_loss, mse_loss
